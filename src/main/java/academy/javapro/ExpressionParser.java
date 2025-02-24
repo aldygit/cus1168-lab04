a2c8a50131687ac7991b2a2490cea78bc6c6409a
aldygit <161101145+aldygit@users.noreply.github.com>
2025-02-24 13:20:32 -0500

    finished homework

--- src/main/java/academy/javapro/ExpressionParser.java
+++ src/main/java/academy/javapro/ExpressionParser.java
@@ -1,6 +1,7 @@
 package academy.javapro;
 
 class ExpressionParser {
+
     private final String input;
     private int position;
 
@@ -9,64 +10,62 @@ class ExpressionParser {
         this.position = 0;
     }
 
-    // expr → expr + term
     public double parseExpression() {
-        // TODO: Get the leftmost term value by calling parseTerm()
-        // TODO: While we haven't reached the end of input and current char is '+'
-        // TODO: Increment position to skip the '+' operator
-        // TODO: Get the next term on the right
-        // TODO: Add the right term to our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseExpression");
+        double result = parseTerm();
+        while (position < input.length() && input.charAt(position) == '+') {
+            position++;
+            result += parseTerm();
+        }
+        return result;
     }
 
-    // term → term * factor
     private double parseTerm() {
-        // TODO: Get the leftmost factor value by calling parseFactor()
-        // TODO: While we haven't reached the end of input and current char is '*'
-        // TODO: Increment position to skip the '*' operator
-        // TODO: Get the next factor on the right
-        // TODO: Multiply the right factor with our running total
-        // TODO: Return the final value
-        throw new UnsupportedOperationException("Implement parseTerm");
+        double result = parseFactor();
+        while (position < input.length() && input.charAt(position) == '*') {
+            position++;
+            result *= parseFactor();
+        }
+        return result;
     }
 
-    // factor → ( expr )
     private double parseFactor() {
-        // TODO: Check if we have an opening parenthesis and haven't reached end of input
-        // TODO: Increment position to skip the opening parenthesis
-        // TODO: Parse the expression inside the parentheses
-        // TODO: Increment position to skip the closing parenthesis
-        // TODO: Return the result of the parenthesized expression
-        // TODO: If no parentheses, parse and return a number
-        throw new UnsupportedOperationException("Implement parseFactor");
+        if (position < input.length() && input.charAt(position) == '(') {
+            position++;
+            double result = parseExpression();
+            if (position < input.length() && input.charAt(position) == ')') {
+                position++;
+            } else {
+                throw new IllegalArgumentException("Mismatched parentheses at position " + position);
+            }
+            return result;
+        }
+        return parseNumber();
     }
 
-    // Parse a numeric value
     private double parseNumber() {
-        // TODO: Create a StringBuilder to collect digits
-        // TODO: While we haven't reached the end and current char is digit or decimal point
-        // TODO: Append the current digit to our number string
-        // TODO: Move to next character
-        // TODO: Convert the string of digits to a double and return it
-        throw new UnsupportedOperationException("Implement parseNumber");
+        StringBuilder sb = new StringBuilder();
+        while (position < input.length() && (Character.isDigit(input.charAt(position)) || input.charAt(position) == '.')) {
+            sb.append(input.charAt(position++));
+        }
+        if (sb.length() == 0) {
+            throw new IllegalArgumentException("Unexpected character at position " + position);
+        }
+        return Double.parseDouble(sb.toString());
     }
 
     public static void main(String[] args) {
-        // Test cases
         String[] testCases = {
-                "2 + 3 * (4 + 5)",    // Complex expression with parentheses
-                "2 + 3 * 4",          // Basic arithmetic with precedence
-                "(2 + 3) * 4",        // Parentheses changing precedence
-                "2 * (3 + 4) * (5 + 6)", // Multiple parentheses
-                "1.5 + 2.5 * 3"       // Decimal numbers
+            "2 + 3 * (4 + 5)", // Complex expression with parentheses
+            "2 + 3 * 4", // Basic arithmetic with precedence
+            "(2 + 3) * 4", // Parentheses changing precedence
+            "2 * (3 + 4) * (5 + 6)", // Multiple parentheses
+            "1.5 + 2.5 * 3" // Decimal numbers
         };
 
-        // Process each test case
         for (String expression : testCases) {
             System.out.println("\nTest Case: " + expression);
             try {
-                ExpressionParser parser = new ExpressionParser(expression.replaceAll("\\s+", "")); // Remove spaces
+                ExpressionParser parser = new ExpressionParser(expression.replaceAll("\\s+", ""));
                 double result = parser.parseExpression();
                 System.out.println("Result: " + result);
             } catch (Exception e) {
